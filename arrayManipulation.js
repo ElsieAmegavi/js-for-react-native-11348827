c5e9d0a519237aab1ad72dc88b284c02541df5b3
elsieroyalty <139142562+ElsieAmegavi@users.noreply.github.com>
2024-05-19 19:39:01 +0000

    task two completed

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -12,7 +12,28 @@ function processArray(arr){
     return result;
 }
 
+function formatArrayStrings(strings,numbers){
+
+    const result = [];
+    for (let i = 0; i < strings.length; i++){
+        if(numbers[i] % 2 === 0){
+            result[i] = strings[i].toUpperCase();
+        }else{
+            result[i] =strings[i].toLowerCase();
+        }
+    }
+    return result;
+}
+
 const realArray = [1,2,3,4,5,6,7,8,9];
 const resultArray = processArray(realArray);
+
+const realStrings = ["Black" , "Purple" , "Camo" , "Gold" , "Blue" , "Wine" , "White"];
+const resultStrings = formatArrayStrings(realStrings,realArray);
+
+
 console.log("The real array is:", realArray);
 console.log("The final array is:", resultArray);
+
+console.log("The real strings are: ",realStrings);
+console.log("The result strings are:",resultStrings);
